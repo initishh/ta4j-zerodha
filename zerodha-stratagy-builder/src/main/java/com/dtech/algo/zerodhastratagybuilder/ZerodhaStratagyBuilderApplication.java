a637e912cc5e725d9e4de964f2d8b8b8ded6113f
Anand Vaidya <Anand.Vaidya@aurea.com>
2019-11-01 11:47:31 +0530

    1. New project for strategy-building 2. seemless login  with Kite 3. stratagy testing with data in database

---
+++ zerodha-stratagy-builder/src/main/java/com/dtech/algo/zerodhastratagybuilder/ZerodhaStratagyBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.dtech.algo.zerodhastratagybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodhaStratagyBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerodhaStratagyBuilderApplication.class, args);
+    }
+
+}
---
+++ zerodha-stratagy-builder/src/test/java/com/dtech/algo/zerodhastratagybuilder/ZerodhaStratagyBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtech.algo.zerodhastratagybuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhaStratagyBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
